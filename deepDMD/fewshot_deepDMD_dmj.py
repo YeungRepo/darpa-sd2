fa00e51909ccc07df2a62c34423e18df264679c2
Dennis Joshy <dennisjoshy@ResNet-22-94.resnet.ucsb.edu>
2022-01-10 14:43:35 -0800

    Latest changes

--- deepDMD/fewshot_deepDMD_dmj.py
+++ deepDMD/fewshot_deepDMD_dmj.py
@@ -724,14 +724,14 @@ if pre_examples_switch == 23:
   phase_space_stitching = 0;    
 
 
-deep_dict_size = 2;
+deep_dict_size = 1;
 
 if with_control:
   deep_dict_size_control = 5;
   
   
 max_depth = 8;  # 7max_depth 3 works well  
-max_width_limit = 1   ;# 20max width_limit -4 works well 
+max_width_limit = 2   ;# 20max width_limit -4 works well 
 
 min_width_limit = max_width_limit;# use regularization and dropout to trim edges for now. 
 min_width_limit_control =10;
